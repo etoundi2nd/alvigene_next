e6789c76373e723d4ec250acea29bc36d6ad3b56
Frank Etoundi <frank.etoundi@bilendo.de>
2024-07-18 21:21:11 +0200

    hideOffcanvas on order page on initial load

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -3,8 +3,19 @@
 import Link from 'next/link'
 import OrderItemList from '../components/orders/orderItemList'
 import formatPrice from '../utils/formatPrice'
+import { useCart } from '../components/contexts/CartContext'
+import { useEffect, useState } from 'react'
 
 export default function order() {
+    const { pendingOrder, setShowOffcanvas } = useCart()
+    const [offcanvasHiddenOnload, setOffcanvasHiddenOnload] = useState()
+    const shippingCost = 2000
+
+    useEffect(() => {
+        if (!offcanvasHiddenOnload) setShowOffcanvas(false)
+        setOffcanvasHiddenOnload(true)
+    })
+
     return (
         <main className="main">
             <div className="py-2 container">
@@ -18,17 +29,17 @@ export default function order() {
 
                         <div className="d-flex flex-row flex-wrap justify-content-between py-0-5">
                             <span className="pe-1">Total de la commande:</span>
-                            <strong>{formatPrice(1000000000000000000000)}</strong>
+                            <strong>{formatPrice(pendingOrder.calculated_total_with_vat)}</strong>
                         </div>
 
                         <div className="d-flex flex-row flex-wrap justify-content-between py-0-5">
                             <span className="pe-1">Frais de livraison:</span>
-                            <strong>{formatPrice(1000000000000000000000)}</strong>
+                            <strong>{formatPrice(shippingCost)}</strong>
                         </div>
 
                         <div className="d-flex flex-row flex-wrap justify-content-between py-0-5">
                             <span className="pe-1">Total :</span>
-                            <strong>{formatPrice(1000000000000000000000)}</strong>
+                            <strong>{formatPrice(Number(pendingOrder.calculated_total_with_vat) + shippingCost)}</strong>
                         </div>
 
                         <Link href="/api/orders/complete_order" className="btn btn-midnight-blue my-2">
